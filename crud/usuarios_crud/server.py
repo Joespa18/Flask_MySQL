74cef6d14e82fe6d2b68610f6ebb4a972b3f6721
José Sepúlveda <joespa18@gmail.com>
2022-07-15 21:32:02 -0400

    Commit

--- crud/usuarios_crud/server.py
+++ crud/usuarios_crud/server.py
@@ -7,19 +7,46 @@ app = Flask(__name__)
 def index():
     return redirect('/users')
 
-@app.route('/users/new')
-def new_user():
-    return render_template('crear.html')
-
 @app.route('/users')
 def users():
     return render_template('leer.html', users=Usuario.get_all())
 
-@app.route('/users/create', methods=['POST'])
+@app.route('/user/new')
+def new_user():
+    return render_template('crear.html')
+
+@app.route('/user/create', methods=['POST'])
 def create():
     print(request.form)
     Usuario.save(request.form)
     return redirect('/users')
 
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data ={
+        "id":id
+    }
+    return render_template("editar.html", user=Usuario.get_one(data))
+
+@app.route('/user/show/<int:id>')
+def show(id):
+    data ={
+        "id":id
+    }
+    return render_template("show_user.html", user=Usuario.get_one(data))
+
+@app.route('/user/update', methods=['POST'])
+def update():
+    Usuario.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data={
+        'id':id
+    }
+    Usuario.destroy(data)
+    return redirect('/users')
+
 if __name__=="__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
--- crud/usuarios_crud/users.py
+++ crud/usuarios_crud/users.py
@@ -10,10 +10,13 @@ class Usuario:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+    def full_name(self):
+        return f"{self.nombre} {self.apellido}"
+
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM usuarios;"
-        results = connectToMySQL('usuarios').query_db(query)
+        results = connectToMySQL('db_usuarios').query_db(query)
         users = []
         for u in results:
             users.append(cls(u))
@@ -22,5 +25,21 @@ class Usuario:
     @classmethod
     def save(cls, data):
         query = "INSERT INTO usuarios (nombre, apellido, email) VALUES (%(nombre)s, %(apellido)s, %(email)s);"
-        result = connectToMySQL('usuarios').query_db(query, data)
+        result = connectToMySQL('db_usuarios').query_db(query, data)
         return result
+
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM usuarios WHERE id = %(id)s;"
+        result =connectToMySQL('db_usuarios').query_db(query, data)
+        return cls(result[0])
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE usuarios SET nombre=%(nombre)s, apellido=%(apellido)s, email=%(email)s, updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL('db_usuarios').query_db(query, data)
+
+    @classmethod
+    def destroy(cls,data):
+        query = "DELETE FROM usuarios WHERE id = %(id)s;"
+        return connectToMySQL('db_usuarios').query_db(query, data)
